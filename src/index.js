d523da96f894f44347bac3a742510619b0fd87ba
Simon Dalvai <s.dalvai@noi.bz.it>
2023-04-17 16:55:08 +0200

    show basic view of daily website visits

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-class HelloWorld extends HTMLElement {
+class WebsiteStats extends HTMLElement {
     constructor() {
         super();
 
@@ -7,41 +7,21 @@ class HelloWorld extends HTMLElement {
         // interfer with the host, nor be vulnerable to outside
         // changes --> Solution = SHADOW DOM
         this.shadow = this.attachShadow(
-            {mode: "open"}    // Set mode to "open", to have access to
-                              // the shadow dom from inside this component
+            { mode: "open" }    // Set mode to "open", to have access to
+            // the shadow dom from inside this component
         );
     }
 
-    // Attributes we care about getting values from
-    // Static, because all HelloWorld instances have the same
-    //   observed attribute names
-    static get observedAttributes() {
-        return ['title'];
+    get visits() {
+        // return this.getAttribute("title");
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "https://mobility.api.opendatahub.testingmachine.eu/v2/flat%2Cnode/Website/%2A/latest?limit=200&offset=0&shownull=false&distinct=true&timezone=UTC", false)
+        xhttp.send();
+        let json = JSON.parse(xhttp.response);
+        console.log(json);
+        return json.data[0].scode.split("/")[1].toUpperCase() + " " + json.data[0].mvalue + " - " + json.data[1].scode.split("/")[1].toUpperCase() + " " + json.data[1].mvalue;
     }
 
-    // Override from HTMLElement
-    // Do not use setters here, because you might end up with an endless loop
-    attributeChangedCallback(propName, oldValue, newValue) {
-        console.log(`Changing "${propName}" from "${oldValue}" to "${newValue}"`);
-        if (propName === "title") {
-            this.render();
-        }
-    }
-
-    // We should better use such getters and setters and not
-    // internal variables for that to avoid the risk of an
-    // endless loop and to have attributes in the html tag and
-    // Javascript properties always in-sync.
-    get title() {
-        return this.getAttribute("title");
-    }
-
-    set title(newTitle) {
-        this.setAttribute("title", newTitle)
-    }
-
-    // Triggers when the element is added to the document *and*
-    // becomes part of the page itself (not just a child of a detached DOM)
     connectedCallback() {
         this.render();
     }
@@ -50,15 +30,17 @@ class HelloWorld extends HTMLElement {
         this.shadow.innerHTML = `
             <style>
                 h1 {
-                    color: red;
+                    color: black;
+                    font-size: 34px;
+                    font-family: 'Source Sans Pro',sans-serif;
                 }
             </style>
             <h1>
-                ${this.title}
+                ${this.visits}
             </h1>
         `;
     }
 }
 
 // Register our first Custom Element named <hello-world>
-customElements.define('hello-world', HelloWorld);
+customElements.define('website-stats', WebsiteStats);
